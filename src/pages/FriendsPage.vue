d4cc94f1ca80d27d6390eec207e72c516ba81dbe
Luke Diebold <luke@ldiebold.com>
2024-06-20 19:37:50 +1000

    end of lesson 6

---
+++ src/pages/FriendsPage.vue
@@ -0,0 +1,9 @@
+<script setup>
+
+</script>
+
+<template>
+  <q-page padding>
+    FriendsPage
+  </q-page>
+</template>
--- src/router/routes.js
+++ src/router/routes.js
@@ -3,7 +3,14 @@ const routes = [
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/IndexPage.vue') }
+      {
+        path: '',
+        component: () => import('pages/IndexPage.vue')
+      },
+      {
+        path: 'friends',
+        component: () => import('pages/FriendsPage.vue')
+      }
     ]
   },
 
