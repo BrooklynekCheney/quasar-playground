5b0c0fce1972a8f7c6d279fa1f9406a4042493a9
Luke Diebold <luke@ldiebold.com>
2024-07-17 11:52:04 +1000

    end of lesson 9.1

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,12 +1,42 @@
 <script setup>
+import { ref } from 'vue'
+import { friends } from './friends'
 
+const columns = [
+  {
+    name: 'name',
+    field: 'name',
+    label: 'Name',
+    align: 'left'
+  },
+  {
+    name: 'email',
+    field: 'email',
+    label: 'Email',
+    align: 'left'
+  }
+]
+
+const fullscreen = ref(false)
 </script>
 
 <template>
   <q-page class="flex flex-center">
     <q-btn
-      label="Hello Quasar!"
-      color="red"
+      label="Fullscreen"
+      @click="fullscreen = !fullscreen"
+    />
+
+    <q-table
+      v-model:fullscreen="fullscreen"
+      :columns
+      :rows="friends"
+      separator="cell"
+      title="Friends"
+      flat
+      bordered
+      square
+      @row-click="(_evt, row) => console.log(row)"
     />
   </q-page>
 </template>
---
+++ src/pages/friends.js
@@ -0,0 +1,6 @@
+export const friends = [
+  { id: 'dd5c3aea-2828-4a29-ac37-06fa9de3fb5c', name: 'Lugu', email: 'luke@ldiebold.com' },
+  { id: '1aecd86f-6e93-49b3-bd60-df7814f1130b', name: 'Shannen', email: 'shannen@ldiebold.com' },
+  { id: '45d02a13-5668-4d4e-8e6b-97432444e5ac', name: 'Panda', email: 'panda@ldiebold.com' },
+  { id: '495a2089-dce8-4ece-a96b-4aa9667effe6', name: 'Lily', email: 'lily@ldiebold.com' }
+]
