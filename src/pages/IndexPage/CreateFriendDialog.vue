3d605d200a1d4b2f5fa7009481dda499c8b10cae
Luke Diebold <luke@ldiebold.com>
2024-07-17 21:43:54 +1000

    end of lesson 11

---
+++ src/pages/IndexPage/CreateFriendDialog.vue
@@ -0,0 +1,58 @@
+<script setup>
+const form = defineModel('form', { type: Object })
+
+const emit = defineEmits(['create'])
+</script>
+
+<template>
+  <q-dialog>
+    <q-card>
+      <q-card-section>
+        <q-form
+          class="q-gutter-y-md"
+          @submit="emit('create')"
+        >
+          <!-- name: text input -->
+          <q-input
+            v-model="form.name"
+            label="Name"
+            filled
+          />
+          <!-- email: text input -->
+          <q-input
+            v-model="form.email"
+            label="Email"
+            filled
+          />
+          <!-- age: number input -->
+          <q-input
+            v-model.number="form.age"
+            label="Age"
+            type="number"
+            filled
+          />
+          <!-- isHuman: checkbox -->
+          <q-checkbox
+            v-model="form.is_human"
+            label="Is Human"
+          />
+          <!-- operating_system: select -->
+          <q-select
+            v-model="form.operating_system"
+            outlined
+            :options="['Windows', 'Mac', 'Linux']"
+          />
+          <!-- color: color picker -->
+          <q-color v-model="form.color" />
+          <!-- Create button -->
+          <q-btn
+            label="Create"
+            color="primary"
+            class="full-width"
+            type="submit"
+          />
+        </q-form>
+      </q-card-section>
+    </q-card>
+  </q-dialog>
+</template>
---
+++ src/pages/IndexPage/IndexPage.vue
@@ -0,0 +1,59 @@
+<script setup>
+import { ref } from 'vue'
+import { useLocalStorage } from '@vueuse/core'
+import { uid } from 'quasar'
+import { mdiPlus } from '@quasar/extras/mdi-v7'
+import CreateFriendDialog from './CreateFriendDialog.vue'
+
+const columns = [
+  {
+    name: 'name',
+    field: 'name',
+    label: 'Name',
+    align: 'left',
+    sortable: true
+  },
+  {
+    name: 'email',
+    field: 'email',
+    label: 'Email',
+    align: 'left'
+  }
+]
+const friends = useLocalStorage('friends', {})
+const form = ref({})
+
+function createFriend () {
+  const id = uid()
+  form.value.id = id
+  friends.value[id] = form.value
+  showCreateDialog.value = false
+}
+
+const showCreateDialog = ref(false)
+</script>
+
+<template>
+  <q-page class="row flex-center">
+    <q-table
+      :columns
+      :rows="Object.values(friends)"
+      title="Friends"
+    >
+      <template #top-right>
+        <q-btn
+          :icon="mdiPlus"
+          round
+          color="primary"
+          @click="showCreateDialog = true"
+        />
+      </template>
+    </q-table>
+
+    <CreateFriendDialog
+      v-model="showCreateDialog"
+      v-model:form="form"
+      @create="createFriend()"
+    />
+  </q-page>
+</template>
