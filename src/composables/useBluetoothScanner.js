7c3c1d12f406a14ad6e11f8fa8f6e9ae44b1ff39
Brooklyne Cheney <59592827+BrooklynekCheney@users.noreply.github.com>
2025-05-13 12:19:17 -0700

    Get devices into store pronto

--- src/composables/useBluetoothScanner.js
+++ src/composables/useBluetoothScanner.js
@@ -1,31 +1,80 @@
+'use strict'
 import { BleClient } from '@capacitor-community/bluetooth-le'
 import { ref } from 'vue'
+
+// const { BrowserWindow } = require('electron');
+// let win;
+
+// let win = null
+// var callbackForBluetoothEvent = null
 
 export function useBluetoothScanner () {
   const scanResults = ref([])
   const scanning = ref(false)
+  const deviceIds = []
 
   async function scan () {
     await BleClient.initialize()
 
-    scanning.value = true
+    console.log(BleClient.getDevices(deviceIds))
+    //  await function  createWindow () {
+    //       win = new BrowserWindow({
+    //         width: 800,
+    //         height: 600,
+    //         webPreferences: {
+    //           nodeIntegration: true,
+    //           contextIsolation: false,
+    //         },
+    //       });
 
-    BleClient.requestLEScan(
-      {},
-      result => {
-        scanResults.value.push(result)
+    //     }
+    // win.webContents.on('select-bluetooth-device', (event, deviceList, callback) => {
+    //   event.preventDefault()
+    //   console.log(deviceList)
+    //   // let bluetoothDeviceList = deviceList
+    //   callbackForBluetoothEvent = callback
+
+    //   win.webContents.send('channelForBluetoothDeviceList')
+    // })
+    if (navigator.bluetooth && navigator.bluetooth.requestDevice) {
+      try {
+        navigator.bluetooth.requestDevice({
+          acceptAllDevices: true
+
+          // Optional filters:
+          //   filters: [{services: ['battery_service']}]
+        })
+        // win.webContents.on('select-bluetooth-device', (event, deviceList) => {
+        //   event.preventDefault(); //important, otherwise first available device will be selected
+        //   console.log(deviceList); })
+
+        console.log('Scan st')
+
+        // Stop scanning after a timeout (optional)
+        setTimeout(async () => {
+          await BleClient.stopLEScan()
+          console.log('Scan stopped')
+        }, 10000) // Stop after 10 seconds
+        console.log('Scan started:', scan)
+      } catch (error) {
+        console.error('Error during scanning:', error)
       }
-    )
-
-    setTimeout(async () => {
-      await BleClient.stopLEScan()
-      scanning.value = false
-    }, 7_000)
+    } else {
+      // Function does not exist, handle accordingly
+      console.warn('requestLEScan is not supported in this browser.')
+      // Provide alternative functionality or inform the user
+    }
   }
 
   return {
     scanResults,
     scanning,
     scan
+    // selectFromDevicePicker,
+    // closeDevicePicker,
+    // cancelDevicePicker,
+    // discoverDevice,
+    // startDevicepicker,
+    // createWindow,
   }
 }
